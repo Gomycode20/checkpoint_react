3b11e2d46824a53181a589706506204611cf0674
azzasfargandoura <azzasfargandoura1@gmail.com>
2025-04-25 15:26:58 +0200

    chez mamie

--- checkpoint_react/src/components/Cards.jsx
+++ checkpoint_react/src/components/Cards.jsx
@@ -3,12 +3,12 @@ import Card from 'react-bootstrap/Card';
 
 function MyCards(props) {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ fontFamily: 'script MJ', fontSize: '18px', width: '18rem' }}>
       <Card.Img variant="top" src={props.img} />
       <Card.Body>
         <Card.Title> {props.title} </Card.Title>
         <Card.Text> {props.text} </Card.Text>
-        <Button variant="primary">Passez à la commande</Button>
+        <Button style={{backgroundColor: '#faedcd', color:'black', borderColor:'black'}}>Passez à la commande</Button>
       </Card.Body>
     </Card>
   );
--- checkpoint_react/src/components/Navbar.jsx
+++ checkpoint_react/src/components/Navbar.jsx
@@ -5,31 +5,25 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Mynavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" style={{ backgroundColor: '#faedcd', fontFamily: 'script MJ' }}>
       <Container>
-        <Navbar.Brand href="#home">L'Atelier de Sadok</Navbar.Brand>
+        <Navbar.Brand href="#home">Chez Mamie</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-          <NavDropdown title="Boutique en ligne" id="basic-nav-dropdown">
+            <NavDropdown title="Boutique en ligne" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Pâtisserie</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Epicerie Fine
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Epicerie Fine</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Coffrets cadeaux</NavDropdown.Item>              
             </NavDropdown>
             <Nav.Link href="#link"> Chocolats & Macarons </Nav.Link>
             <Nav.Link href="#link"> Mariage & Naissance </Nav.Link>
             <NavDropdown title="Contact" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Email</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Instagram
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Instagram</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Whatsapp</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Our website
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Our website</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
